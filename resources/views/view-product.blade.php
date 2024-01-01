49834034a0ef1f896803cbcc2346a497473cb930
c3585124 <m.riaz3363@student.leedsbeckett.ac.uk>
2024-01-01 17:51:33 +0000

    Image Upload (for Add Image) now working and displaying correctly. If admin user does not upload an image, the picsums placeholder is displayed, else the uploaded image is displayed

--- resources/views/view-product.blade.php
+++ resources/views/view-product.blade.php
@@ -5,16 +5,21 @@
         <h1 class="text-3xl font-semibold">{{ $product->title }}</h1>
         <p class="text-lg text-gray-600">{{ $product->artist }}</p>
         <p class="text-xl font-bold text-gray-800 mt-2 my-5">@money($product->price)</p>
-        <img src="{{ $product->image_path }}" class="mb-4 w-full h-auto">
 
-        <!-- will check if the user is logged in, if so then the "buy" button is displayed. If not logged in, then there is no "Buy" button ------ consider adding JS method for a button alert for guests advising them to register/login if they want to purchase -->
+        <!-- If the image_path is set to the placeholder/null, then the placeholder will be shown -->
+        @if($product->image_path !== 'https://picsum.photos/1200/800')
+            <img src="{{ asset('storage/images/' . $product->image_path) }}" class="mb-4 w-full h-auto">
+        @else
+            <!-- Else, it displays the uploaded file -->
+            <img src="{{ $product->image_path }}" class="mb-4 w-full h-auto">
+        @endif
+
+        <!-- Will check if the user is logged in, if so then the "buy" button is displayed. If not logged in, then there is no "Buy" button ------ consider adding JS method for a button alert for guests advising them to register/login if they want to purchase -->
         @can('purchase-product')
         <button value="{{ $product->id }}" class="bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded-full buy-product">Buy</button>
         @endcan
         @can('edit-product')
         <button value="{{ $product->id }}" class="bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded-full update-product">Edit Product</button>
         @endcan
-
-        <!-- A delete product button for admin users could be here -->
     </div>
 </x-app-layout>
