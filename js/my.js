593abdb1ae97b18af9d1391229469fd2689a0c76
Awais Riaz <74641603+WasiG-619@users.noreply.github.com>
2024-02-03 22:31:15 +0000

    Created symlink to public dir for Github Pages

---
+++ js/my.js
@@ -0,0 +1,74 @@
+window.onload = function () {
+
+    document.addEventListener('change', e => {
+        if (e.target.matches('select.select-box')) {
+            filterByProductType(e.target.value);
+        }
+    });
+
+    document.addEventListener('click', e => {
+        if (e.target.matches('button.select-product')) {
+            getProductByID(e.target.value);
+        }
+        if (e.target.matches('button.update-product')) {
+            updateProductByID(e.target.value);
+        }
+        if (e.target.matches('button.buy-product')) {
+            buyProductByID(e.target.value);
+        }
+        if (e.target.matches('button.delete-product')) {
+            deleteProductByID(e.target.value);
+        }
+    });
+}
+
+function buyProductByID(id) {
+    alert('product purchased');
+    window.location = "/product/";
+
+}
+
+function getProductByID(id) {
+    window.location = "/product/" + id;
+}
+
+function updateProductByID(id) {
+    window.location = "/product/" + id + "/edit";
+}
+
+async function filterByProductType(id) {
+    if (id == 0) window.location = "/products/"
+    else {
+        try {
+            const response = await axios.get('/filter',
+                {params: {producttype: id}}
+            );
+
+            var filter = document.getElementById("productlist");
+            filter.innerHTML = response.data;
+            var pagination = document.getElementById("pagination");
+            pagination.innerHTML = "<br><br>";
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+
+async function deleteProductByID(id) {
+    try {
+        const response = await axios.delete('/product/' + id);
+
+        if (response.data.message === 'Product deleted') {
+            alert('Successfully Deleted'); // not generating an alert for some reason.....
+            window.location = '/product';
+        } else { 
+        }
+    } catch (error) {
+            console.error(error);
+        }
+}
+
+
+
+
